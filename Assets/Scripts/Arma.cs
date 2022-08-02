7b1a8d4ed2e0950dfb8120d538a8430255701e59
Fabiano De Paula <fabiano.rpaula@gmail.com>
2022-08-02 16:46:45 -0300

    Atualizado 02-08-2022

---
+++ Assets/Scripts/Arma.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arma : MonoBehaviour
+{
+    public bool DanoTiro()
+    {
+        RaycastHit Bateu;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Bateu, 5f))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * Bateu.distance, Color.yellow);
+            Debug.Log("Bati Em Alguma Coisa");
+            Destroy(Bateu.collider.gameObject);
+            return true;
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 5, Color.white);
+            Debug.Log("Não Bati Em nada");
+            return false;
+        }
+    }
+
+}
+
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    private Rigidbody Corpo;
+    private Transform Tcorpo;
+    private AudioSource MeusPassos;//para ouvir passos
+    public float sensibilidade = 1000f;
+    public float speed = 4;
+
+    public GameObject ArmaDeFogo;
+
+
+
+    void Start()
+    {
+        Corpo = GetComponent<Rigidbody>();
+        Tcorpo = GetComponent<Transform>();
+        MeusPassos = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        float movZ = Input.GetAxis("Vertical") * speed;
+        float movX = Input.GetAxis("Horizontal") * 2;
+        
+        if(movZ != 0) {
+            MeusPassos.volume = 1;
+            GetComponent<Animator>().SetBool("Andando", true);
+        }
+        else {
+            MeusPassos.volume = 0;
+            GetComponent<Animator>().SetBool("Andando", false);
+        }
+
+        if(speed == 12) { MeusPassos.pitch = 1.3f;}
+        else { MeusPassos.pitch = 1; }
+
+        if (Input.GetKey(KeyCode.LeftShift))  { speed = 12;}
+        else { speed = 4;}
+
+        Vector3 posicaoCorrigida = movX * transform.right + movZ * transform.forward;
+        Corpo.velocity = new Vector3(posicaoCorrigida.x, Corpo.velocity.y, posicaoCorrigida.z);
+        //GIRAR
+        float mouseX = Input.GetAxis("Mouse X") * sensibilidade * Time.deltaTime; ;
+        Tcorpo.Rotate(Vector3.up * mouseX);
+
+
+        //ATIRANDO
+        if (Input.GetMouseButtonDown(0))
+        {
+            Atirar();
+            GetComponent<Animator>().SetTrigger("Atirar");
+        }
+
+
+    }
+
+    void Atirar()
+    {
+        if (ArmaDeFogo.GetComponent<Arma>().DanoTiro())
+        {
+            Debug.Log("Acertei");
+        }
+        else
+        {
+            Debug.Log("Errei");
+        }
+
+        
+    }
+
+}
